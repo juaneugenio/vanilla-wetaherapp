35d03fe14300af548ea3983d2aeba5350b231821
juaneugenio <leuviah77@gmail.com>
2024-11-13 18:08:57 +0100

    first commit with folder structure

---
+++ scripts/main.js
@@ -0,0 +1,5 @@
+/** @format */
+
+const weatherContainer = document.getElementById("weather-results");
+const cityInput = document.getElementById("city").value;
+console.log("👉 Line-3 ▶︎▶︎", "cityInput");
